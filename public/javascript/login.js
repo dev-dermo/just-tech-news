3e64cc9fd3458f780efeec3485549ef5d262d989
Diarmuid Murphy <diarmuid.com@gmail.com>
2021-09-07 23:18:26 -0700

    signup

---
+++ public/javascript/login.js
@@ -0,0 +1,17 @@
+function signupFormHandler(event) {
+	event.preventDefault();
+
+	const username = document.querySelector('#username-signup').value.trim();
+	const email = document.querySelector('#email-signup').value.trim();
+	const password = document.querySelector('#password-signup').value.trim();
+
+	if (username && email && password) {
+		fetch('/api/users', {
+			method: 'POST',
+			body: JSON.stringify({ username, email, password }),
+			headers: { 'Content-Type': 'application/json' }
+		}).then(res => console.log(res));
+	}
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
